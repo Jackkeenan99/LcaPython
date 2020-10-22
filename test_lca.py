fdc710d83fae529c780b82d4b8443a31421b9b81
Jackkeenan99 <72086159+Jackkeenan99@users.noreply.github.com>
2020-10-22 23:04:37 +0100

    change test condition

--- test_lca.py
+++ test_lca.py
@@ -7,7 +7,7 @@ class Testlca(unittest.TestCase):
 
         root = LowCA.Node(1)
         result = LowCA.findLCA(root, 4, 5)
-        self.assertEquals(-1, -1) #only root in tree
+        self.assertEqual(-1, -1) #only root in tree
         root.left = LowCA.Node(2)
         root.right = LowCA.Node(3)
         root.left.left = LowCA.Node(4)
@@ -15,4 +15,4 @@ class Testlca(unittest.TestCase):
         root.right.left = LowCA.Node(6)
         root.right.right = LowCA.Node(7)
         result = LowCA.findLCA(root, 4, 5)
-        self.assertEquals(2, result)
+        self.assertEqual(2, result)
